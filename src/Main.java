899e59af383c505107977a6820f5849917da47e8
apella1 <johncollons5@gmail.com>
2023-05-17 11:09:19 +0300

    Add primitive data types

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,15 @@ public class Main {
     public static void main(String[] args) {
         int runningSpeed = 100;
         int daysOfTheYear = 365;
+        byte dayOfTheWeek = 7;
+        short marksScored = 87;
+        long depthOfLakeVictoria = 53232522;
+        double currentWeight = 67.4;
+        boolean hasWonAnyMatch = false;
+        char grade = 'A';
+        String dayOverview = "Today is the big match day";
         System.out.println(daysOfTheYear);
         System.out.println("My running speed is " + runningSpeed);
-        System.out.println("The number of accepted resume is " + ACCEPTED_RESUMES);
+        System.out.println("The number of accepted resumes is " + ACCEPTED_RESUMES);
     }
 }
