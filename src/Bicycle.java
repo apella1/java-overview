dea7d0947ab9ef907710ae743bcdbd4f9a25d222
apella1 <johncollons5@gmail.com>
2023-05-19 09:54:12 +0300

    Add interface definition

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -1,26 +1,35 @@
-class Bicycle {
+interface Bike {
+    void changeCadence(int newValue);
+
+    void speedUp(int newValue);
+
+    void applyBrakes(int decrement);
+
+    void changeGear(int newValue);
+}
+
+class Bicycle implements Bike {
     int cadence = 0;
     int speed = 0;
     int gear = 0;
 
-    void changeCadence(int newValue) {
+    public void changeCadence(int newValue) {
         cadence = newValue;
     }
 
-    void changeGear(int newValue) {
+    public void changeGear(int newValue) {
         gear = newValue;
     }
 
-    void speedUp(int increment) {
+    public void speedUp(int increment) {
         speed = speed + increment;
     }
 
-    void applyBrakes(int decrement) {
+    public void applyBrakes(int decrement) {
         speed = speed - decrement;
-}
+    }
 
     void printStates() {
         System.out.println("Cadence: " + cadence + " Speed: " + speed + " Gear " + gear);
     }
 }
-
