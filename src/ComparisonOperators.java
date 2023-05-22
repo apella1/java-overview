2393a44a14d74daa66d74860f3a540ae429adc6d
apella1 <johncollons5@gmail.com>
2023-05-22 09:47:31 +0300

    Add comparison operators

---
+++ src/ComparisonOperators.java
@@ -0,0 +1,14 @@
+public class ComparisonOperators {
+    public static void main(String[] args) {
+        int firstScore = 3;
+        int secondScore = 8;
+
+        if (firstScore == secondScore)
+            System.out.println("First score is equal to second score");
+        else {
+            System.out.println("The scores aren't equal");
+        }
+
+        // the other equality operators are >, >=, <=, !=, <
+    }
+}
