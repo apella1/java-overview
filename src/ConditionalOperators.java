0073e5763de03fb091819f19431dd5b8c23496c1
apella1 <johncollons5@gmail.com>
2023-05-22 09:56:05 +0300

    Add logical AND and OR

---
+++ src/ConditionalOperators.java
@@ -0,0 +1,22 @@
+public class ConditionalOperators {
+    // the logical conditional operators have a short-circuiting effect
+    // meaning the second argument is computed only if necessary
+    public static void main(String[] args) {
+        int firstValue = 3;
+        int secondValue = 4;
+
+        if ((firstValue == 3) && (secondValue == 4)) {
+            System.out.println("First value is 3 AND second value is 4");
+        }
+        if ((firstValue == 3) || (secondValue ==4)) {
+            System.out.println("Fist value is 3 OR second value is 4");
+        }
+
+        int mathsScore = 88;
+        int secondMathsScore = 77;
+        boolean hasPassed = true;
+        int result;
+        result = hasPassed ? mathsScore : secondMathsScore;
+        System.out.println(result);
+    }
+}
