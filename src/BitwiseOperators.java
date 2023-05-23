99108e3b0c6d5f10a96d4a5b53d9a01fd503dbdc
apella1 <johncollons5@gmail.com>
2023-05-23 06:37:50 +0300

    Add bitwise operators

---
+++ src/BitwiseOperators.java
@@ -0,0 +1,9 @@
+public class BitwiseOperators {
+    // not commonly used
+    // <<, >>, >>>, &, ^, |
+    public static void main(String[] args) {
+        int bitmask = 0x000F;
+        int val = 0x2222;
+        System.out.println(val & bitmask);
+    }
+}
