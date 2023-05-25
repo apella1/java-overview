bccd2c18b301b4ee007c9348e2aa157a256d255c
apella1 <johncollons5@gmail.com>
2023-05-25 08:37:07 +0300

    Add continue branching with a label

---
+++ src/ContinueWithLabelDemo.java
@@ -0,0 +1,24 @@
+public class ContinueWithLabelDemo {
+    public static void main(String[] args) {
+        String phrase = "Look for a substring in me.";
+        String sub = "rng";
+        boolean foundSub = false;
+
+        int max = phrase.length() - sub.length();
+        test:
+        for (int i = 0; i <= max; i++) {
+            int n = sub.length();
+            int j = i;
+            int k = 0;
+
+            while (n-- != 0) {
+                if (phrase.charAt(j++) != sub.charAt(k++)) {
+                    continue test;
+                }
+            }
+            foundSub = true;
+            break;
+        }
+        System.out.println(foundSub ? "Found it." : "Didn't find it.");
+    }
+}
