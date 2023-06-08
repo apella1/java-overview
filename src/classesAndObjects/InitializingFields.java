139214411bb41b6f32d29e1c5df4716e5444ba53
apella1 <johncollons5@gmail.com>
2023-06-08 10:13:58 +0300

    Add field initialization techniques for classs members

---
+++ src/classesAndObjects/InitializingFields.java
@@ -0,0 +1,8 @@
+package classesAndObjects;
+
+public class InitializingFields {
+    public static int capacity = 100;
+    private boolean isFull = false;
+
+    // fields can also be initialized using static and private static blocks
+}
